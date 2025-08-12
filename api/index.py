af5fdf20579bb542c788aebee09b6995c01cd64c
Ana Paula <anapsskj@gmail.com>
2025-08-12 18:02:13 -0400

    readme

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from api.endpoints import router
-from api.auth import router as auth_router
-
-app = FastAPI()
-
-app.include_router(router, prefix="/api/v1")
-app.include_router(auth_router, prefix="/api/v1")
